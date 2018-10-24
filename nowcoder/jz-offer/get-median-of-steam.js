af291589b3d88011a6682e51a55142e5632ef245
Sojourn2017 <1195110630@qq.com>
2018-10-24 23:19:07 +0800

    2018-10-24

--- nowcoder/jz-offer/get-median-of-steam.js
+++ nowcoder/jz-offer/get-median-of-steam.js
@@ -10,25 +10,25 @@
 // 使用GetMedian()方法获取当前读取数据的中位数。
 
 let arr = [];
-function Insert(num){
-    let index = arr.length - 1;
-    arr.push(num);
-    for (let i = index; i >= 0; i--) {
-        if (arr[i] > num) {
-            [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
-        } else {
-            break;
-        }
+function Insert(num) {
+  let index = arr.length - 1;
+  arr.push(num);
+  for (let i = index; i >= 0; i--) {
+    if (arr[i] > num) {
+      [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+    } else {
+      break;
     }
+  }
 }
-function GetMedian(){
-    let len = arr.length;
-    if (len <= 0) return;
-    let mid = Math.floor((len - 1) / 2);
-    return len % 2 == 0 ? (arr[mid] + arr[mid + 1]) / 2 : arr[mid];
+function GetMedian() {
+  let len = arr.length;
+  if (len <= 0) return;
+  let mid = Math.floor((len - 1) / 2);
+  return len % 2 == 0 ? (arr[mid] + arr[mid + 1]) / 2 : arr[mid];
 }
 
-let data = [5,2,3,4,1,6,7,0,8];
+let data = [5, 2, 3, 4, 1, 6, 7, 0, 8];
 
 for (let i = 0, len = data.length; i < len; i++) {
   Insert(data[i]);
--- nowcoder/jz-offer/has-path.js
+++ nowcoder/jz-offer/has-path.js
@@ -4,7 +4,7 @@
 // 请设计一个函数，用来判断在一个矩阵中是否存在一条包含某字符串所有字符的路径。
 // 路径可以从矩阵中的任意一个格子开始，每一步可以在矩阵中向左，
 // 向右，向上，向下移动一个格子。如果一条路径经过了矩阵中的某一个格子，
-// 则之后不能再次进入这个格子。 例如 a b c e s f c s a d e e 
+// 则之后不能再次进入这个格子。 例如 a b c e s f c s a d e e
 // 这样的3 X 4 矩阵中包含一条字符串"bcced"的路径，
 // 但是矩阵中不包含"abcb"路径，因为字符串的第一个字符b占据了矩阵中的第一行第二个格子之后，
 // 路径不能再次进入该格子
@@ -29,17 +29,26 @@ function hasPath(matrix, rows, cols, path) {
 
 function hasPathCore(matrix, rows, cols, path, strIndex, curX, curY, flag) {
   let index = curX * cols + curY;
-  if (curX < 0 || curX >= rows || curY < 0 || curY >= cols || matrix[index] !== path[strIndex] || flag[index] == 1) return false;
+  if (
+    curX < 0 ||
+    curX >= rows ||
+    curY < 0 ||
+    curY >= cols ||
+    matrix[index] !== path[strIndex] ||
+    flag[index] == 1
+  )
+    return false;
   if (strIndex == path.length - 1) return true;
   flag[index] = 1;
   if (
-    hasPathCore(matrix, rows, cols, path, strIndex + 1, curX, curY + 1, flag) || 
-    hasPathCore(matrix, rows, cols, path, strIndex + 1, curX, curY - 1, flag) || 
-    hasPathCore(matrix, rows, cols, path, strIndex + 1, curX + 1, curY, flag) || 
+    hasPathCore(matrix, rows, cols, path, strIndex + 1, curX, curY + 1, flag) ||
+    hasPathCore(matrix, rows, cols, path, strIndex + 1, curX, curY - 1, flag) ||
+    hasPathCore(matrix, rows, cols, path, strIndex + 1, curX + 1, curY, flag) ||
     hasPathCore(matrix, rows, cols, path, strIndex + 1, curX - 1, curY, flag)
-  ) return true;
+  )
+    return true;
   flag[index] = 0;
 }
 
-var a = hasPath('abcesfcsadee', 3, 4, 'bcced');
+var a = hasPath("abcesfcsadee", 3, 4, "bcced");
 console.log(a);
